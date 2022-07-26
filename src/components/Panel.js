121fb20d4bb2edcab991826991aed98a65692b52
Yuliia Burko <yuliia.burko@gmail.com>
2022-07-26 11:30:47 +0200

    added examples

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,3 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Panel = () => {};
+const styles = {
+  outline: '1px solid tomato',
+};
+
+const Panel = ({ title, children }) => (
+  <div style={styles}>
+    {title && <h2>{title}</h2>} // если true слева, рендрится то что справа !!!
+    {children}
+  </div>
+);
+
+// если не обязательные значения, то поставить значения по умолчанию !!
+Panel.defaultProps = {
+  title: '',
+  children: [],
+};
+
+Panel.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Panel;
